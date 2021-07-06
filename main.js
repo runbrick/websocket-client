9391f14a67f9a0bd4a078235115721107674367a
奔跑的砖头 <zhouwei9280@gmail.com>
2021-07-06 15:58:45 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const {app, BrowserWindow, Menu, dialog, ipcMain} = require("electron");
+
+/**
+ * 创建窗口
+ */
+function createWindow() {
+    const bw = new BrowserWindow({
+        width: 400,
+        height: 580,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+        // useContentSize: false, // 禁止最大化
+        // resizable: false // 禁止调整大小
+    })
+    // 打开调试
+    bw.webContents.openDevTools()
+    bw.loadFile("index.html")
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+// 设置没有菜单
+Menu.setApplicationMenu(null)
+
+ipcMain.on("errmsg", function (event, arg) {
+    dialog.showMessageBox({
+        type: "error",
+        message: arg
+    })
+    return false;
+})
+
+ipcMain.on("sucmsg", function (event, arg) {
+    dialog.showMessageBox({
+        type: "info",
+        message: arg
+    })
+    return false;
+})
